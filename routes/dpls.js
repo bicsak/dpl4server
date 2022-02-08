c6abad49d20d2e3d496bb5e65c20bd9d5bd70768
Malaj <bicsak@gmx.net>
2022-02-08 01:03:17 +0100

    merge seating dienst

--- routes/dpls.js
+++ routes/dpls.js
@@ -17,22 +17,42 @@ function verifyToken(req,res,next) {
    }
 }
 
-router.get('/:mts', verifyToken, async function(req, res) {
+router.get('/:section/:mts', verifyToken, async function(req, res) {
    
    jwt.verify(req.token, process.env.JWT_PASS, async function (err,authData) {
       if (err) 
          res.sendStatus(403);
       else {
-         let oneDpl = await Dpl.findOne({
+         if ( req.params.section !== authData.s && authData.s !== 'all' ) res.sendStatus(403);
+         
+         let dpl = await Dpl.findOne({
             o: authData.o,
             weekBegin: new Date(req.params.mts*1000),
-            s: authData.s
+            s: req.params.section
          }).populate('p').populate('w');                  
-               
-         console.log(authData);
+
+         let dplClient = {};         
+         if ( dpl ) {
+            
+            dplClient =  dpl.seatings.map( seating => {               
+               let retVal = Object.assign(
+                  {}, 
+                  seating.toJSON(), 
+                  dpl.w.dienst.find( d => d._id.toString() === seating.d.toString() ).toJSON()
+               );
+
+               delete retVal.instrumentation;
+               delete retVal.d;
+               delete retVal.dienstBegin;
+
+               return retVal;
+            });        
+         }               
+         
          res.json({
             msg: 'Auth success',
-            dpl: oneDpl
+            dplDB: dpl,
+            dplClient: dplClient
          });
       }
    });
