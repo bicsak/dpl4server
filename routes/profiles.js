c494684ede17e1eb2a71f5f779e5884b0b771926
Malaj <bicsak@gmx.net>
2023-02-25 00:37:19 +0100

    get all profiles for scheduler gets only musicians

--- routes/profiles.js
+++ routes/profiles.js
@@ -3,10 +3,35 @@ let router = express.Router();
 const Profile = require('../models/profile');
 
 router.get('/', async function(req, res) {    
-    // TODO if manager: all profiles for this o; if scheduler, only for his section
-    let resp = await Profile.find( { o: req.authData.o } );
+    // if manager: all profiles for this o; if scheduler, only for his section and only musicians
+    let filter = { o: req.authData.o };
+    if ( req.authData.r == 'scheduler' ) {      
+      filter = {
+         ...filter,
+         section: req.authData.s,
+         role: 'musician'
+      };
+    }
+    let resp = await Profile.find( filter ).populate('user');
     console.log(resp);
-    res.json( resp );    
+    res.json( resp.map(
+      doc => {
+         return {
+            _id: doc._id,
+        
+            role: doc.role,
+            manager: doc.manager,
+            section: doc.section,
+            
+            confirmed: doc.confirmed,
+            
+            userId: doc.user.id, 
+            userFn: doc.userFn,
+            userSn: doc.userSn,
+            userEmail: doc.user.email
+         };
+      }
+    ) );    
  });
 
  /*
