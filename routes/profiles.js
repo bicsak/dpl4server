c936d74b4979eb0ed7cb970706bca12f33d5f4e8
Malaj <bicsak@gmx.net>
2023-03-10 22:47:48 +0100

    profile operations skeleton

--- routes/profiles.js
+++ routes/profiles.js
@@ -40,15 +40,28 @@ router.get('/', async function(req, res) {
  });
 
  router.delete('/:id', async function(req, res) {
-   //TODO delete profile request
-   // check if not yet confirmed
+   //TODO delete profile (invitation by manager)
+   // or deny invitation (user denies invitation)
+   // check if not already confirmed
+   // update user doc's profiles field in users collection (delete from array)
+   
  });
-
  
  router.post('/', function(req, res){
     res.send('POST route on profiles');
-    // Create new profile for a specific user; confirmed = false
-    // body contains username (email) and section; if all, create office role, otherwise musician role
+    // TODO
+    // Create new profile doc in profiles collection for a specific user with confirmed = false
+    // request body contains username (email) and section; if section == all, create office role, otherwise musician role
+    // update user doc's profiles field in users collection (add new profile to array)
+    // return new profile as IProfile
+ });
+
+ router.patch('/:id', function(req, res) {
+   //TODO accept invitation to an orchestra as musician/office
+   // modify doc in profiles collection
+   // if role == musician and first profile in the group, add a second profile with scheduler role
+   // update doc in users collection (profiles field)
+   // return the whole profile doc in an array  (IProfile[]) with the scheduler's profile if newly created
  });
  
  //export this router to use in our index.js
