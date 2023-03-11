fd560a44c5e62f215577570640a80cff5eca9ba7
Malaj <bicsak@gmx.net>
2023-03-11 23:23:51 +0100

    create profile invitation

--- routes/profiles.js
+++ routes/profiles.js
@@ -1,6 +1,9 @@
 let express = require('express');
 let router = express.Router();
 const Profile = require('../models/profile');
+const User = require('../models/user');
+
+const { writeOperation } = require('../my_modules/orch-lock');
 
 router.get('/', async function(req, res) {    
     // if manager: all profiles for this o; if scheduler, only for his section and only (confirmed) musicians
@@ -46,17 +49,60 @@ router.get('/', async function(req, res) {
    // update user doc's profiles field in users collection (delete from array)
    
  });
+
+ async function createProfile(session, params) { 
+  console.log(`Role: ${params.role}, sec: ${params.sec}, user: ${params.username}`);        
+
+  let user = User.findOne( {
+    email: params.username
+  }).session(session);  
+
+  const profile = new Profile( {
+    o: params.o,
+    confirmed: false,
+    manager: false,
+    section: params.sec,
+    role: params.role,    
+    userFn: user.fn,
+    userSn: user.sn,
+    userBirthday: user.birthday,
+    email: user.email,
+    user: user._id
+  } );
+  profile.save(session);
+
+  user.profiles.push( {
+    _id: profile._id,
+    o: params.o,
+    role: params.role,
+    manager: false,
+    section: params.sec,
+    confirmed: false
+  } );
+  user.save();
+
+  return profile;  // TODO as IProfile         
+ }
  
- router.post('/', function(req, res){
-    res.send('POST route on profiles');
-    // TODO
+ router.post('/', async function(req, res){    
     // Create new profile doc in profiles collection for a specific user with confirmed = false
     // request body contains username (email) and section; if section == all, create office role, otherwise musician role
     // update user doc's profiles field in users collection (add new profile to array)
     // return new profile as IProfile
+
+    let role = req.body.section == 'all' ? 'office' : 'musician';
+    let result = await writeOperation( req.authData.o, createProfile, {      
+      o: req.authData.o,       
+      role: role,      
+      sec: req.body.section,
+      user: req.body.username
+   });      
+   console.log(`Profile successfully created: ${result}`);      
+       
+   res.json( result );     
  });
 
- router.patch('/:id', function(req, res) {
+ router.patch('/:id', async function(req, res) {
    //TODO accept invitation to an orchestra as musician/office
    // modify doc in profiles collection
    // if role == musician and first profile in the group, add a second profile with scheduler role
