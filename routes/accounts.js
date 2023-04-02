f0d1fd5468fd5973d9d5c3e615e4e1f685cf55ac
Malaj <bicsak@gmx.net>
2023-04-02 23:21:14 +0200

    forgot-pw skeleton

--- routes/accounts.js
+++ routes/accounts.js
@@ -18,9 +18,7 @@ router.post('/sign-up', async (req, res, next) => {
         cutOff.setTime(cutOff.getTime()-3600*1000);
         await User.deleteMany({
             status: 'pending',
-            created_at: {
-                $lt: cutOff
-            }
+            created_at: { $lt: cutOff }
         });
         
         // create user doc with status: 'pending' (default), with new confirmation code + ts (auto)
@@ -66,23 +64,8 @@ router.post('/sign-up', async (req, res, next) => {
                 },
                 // uncomment below to send emails in development/test env:
                 // send: true
-                transport: transporter
-                /*{
-                  jsonTransport: true
-                }*/
-              });
-
-            
-            /*let message = {
-                from: '"Orchesterdienstplan" no-reply@odp.bicsak.net',
-                to: userDoc.email,
-                subject: "Benutzekonto bestätigen",
-                text: `Hallo ${userDoc.fn}, 
-                Du hast eben für ODP registriert. Bitte bestätige dein BEnutzeraccount mit dem link`,
-                html: `<p>Hallo ${userDoc.fn}, </p>
-                 <p>Bitte Benutzerkonto bestätigen</p>`
-            };
-            transporter.sendMail(message); */
+                transport: transporter                
+              });                        
 
             email.send({
                 template: 'signup',
@@ -110,51 +93,77 @@ router.post('/sign-up', async (req, res, next) => {
     try {
         console.log(req.params);
         console.log(req.body);
-        
-        // TODO check if params.token == user's token
+
+        let cutOff = new Date();
+        cutOff.setTime(cutOff.getTime()-3600*1000);
+        await User.deleteMany({
+            status: 'pending',
+            created_at: { $lt: cutOff }
+        });        
+
+        let userDoc = User.findById(req.body.id);        
                 
-        if ( !userDoc) {
+        if ( !userDoc || userDoc.status != 'pending' || userDoc.confirmationCode != req.body.token
+        ) {
             res.status(400).send({
                 msg: 'Verifying not succeeded'
             });
-        } else {
-            // on success send email with link to app route /verify-email (code: token + userId: userDoc._id)            
-            //console.log(`Url: ${req.get('origin')}/verify-email?id=${userDoc.id}&token=${token}`);
-            /*let transporter = nodemailer.createTransport({                
-                host: process.env.MAIL_HOST,                        
-                port: process.env.MAIL_PORT,
-
-                secure: false, // upgrade later with STARTTLS
-                auth: {                          
-                  user: process.env.MAIL_USER,                          
-                  pass: process.env.MAIL_PASS
-                },
-                tls:{
-                    rejectUnauthorized:false  // if on local
-                }
-            });
-            let message = {
-                from: '"Orchesterdienstplan" no-reply@odp.bicsak.net',
-                to: userDoc.email,
-                subject: "Benutzekonto bestätigen",
-                text: `Hallo ${userDoc.fn}, 
-                Du hast eben für ODP registriert. Bitte bestätige dein BEnutzeraccount mit dem link`,
-                html: `<p>Hallo ${userDoc.fn}, </p>
-                 <p>Bitte Benutzerkonto bestätigen</p>`
-            };
-            transporter.sendMail(message); */
-            res.sendStatus(200);
+        } else {            
+            res.json(null);
         }
     }
-
 
     catch (err) {
         return res.status(400).send({
             msg: err
         });
     }
+});
 
-  });
+router.post('/forgot-password', async (req, res, next) => {
+    try {
+        console.log(req.params);
+        console.log(req.body); //contains email
+
+        //TODO
+    }
+
+    catch (err) {
+        return res.status(400).send({
+            msg: err
+        });
+    }
+});
+
+router.post('/validate-reset-token', async (req, res, next) => {
+    try {
+        console.log(req.params);
+        console.log(req.body); // conatains reset token
+
+        //TODO
+    }
+
+    catch (err) {
+        return res.status(400).send({
+            msg: err
+        });
+    }
+});
+
+router.post('/reset-password', async (req, res, next) => {
+    try {
+        console.log(req.params);
+        console.log(req.body); //contains new pw and reset token (validate again!)
+
+       //TODO
+    }
+
+    catch (err) {
+        return res.status(400).send({
+            msg: err
+        });
+    }
+});
 
 
 //export this router to use in our index.js
