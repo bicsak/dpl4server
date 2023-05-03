ccb34913682da96d1296b22327f821c09c0686e2
Malaj <bicsak@gmx.net>
2023-05-04 00:28:15 +0200

    patch season

--- routes/seasons.js
+++ routes/seasons.js
@@ -4,22 +4,43 @@ const Season = require('../models/season');
 const DienstExtRef = require('../models/dienst');
 const Week = require('../models/week');
 
+async function addStat(s) {
+   let countDienst = await DienstExtRef.countDocuments( { season: s._id } );
+   let countCat0 = await DienstExtRef.countDocuments( { season: s._id, category: 0 } );
+   let countCat1 = await DienstExtRef.countDocuments( { season: s._id, category: 1 } );
+   let countPrem = await DienstExtRef.countDocuments( { season: s._id, category: 1, subtype: 1 } );
+   let countWA = await DienstExtRef.countDocuments( { season: s._id, category: 1, subtype: 2 } );
+   let countConc = await DienstExtRef.countDocuments( { season: s._id, category: 1, subtype: 3 } );         
+   let countWeeks = await Week.countDocuments( { season: s._id } );         
+   let ret = Object.assign(s.toJSON(), {countDienst, countCat0, countCat1, countPrem, countWA, countConc, countWeeks});         
+
+   return ret;
+}
+
 router.get('/', async function(req, res) { 
    let response = await Season.find( { o: req.authData.o } );         
-   if ( req.query.full == 'true' ) {
-      console.log('Creating stat for seasons component (manager)');
-      for ( let i = 0; i < response.length; i++ ){
-         let countDienst = await DienstExtRef.countDocuments( { season: response[i]._id } );
-         let countCat0 = await DienstExtRef.countDocuments( { season: response[i]._id, category: 0 } );
-         let countCat1 = await DienstExtRef.countDocuments( { season: response[i]._id, category: 1 } );
-         let countPrem = await DienstExtRef.countDocuments( { season: response[i]._id, category: 1, subtype: 1 } );
-         let countWA = await DienstExtRef.countDocuments( { season: response[i]._id, category: 1, subtype: 2 } );
-         let countConc = await DienstExtRef.countDocuments( { season: response[i]._id, category: 1, subtype: 3 } );         
-         let countWeeks = await Week.countDocuments( { season: response[i]._id } );         
-         response[i] = Object.assign(response[i].toJSON(), {countDienst, countCat0, countCat1, countPrem, countWA, countConc, countWeeks});         
+   if ( req.query.full == 'true' ) {      
+      for ( let i = 0; i < response.length; i++ ){        
+         response[i] = await addStat(response[i]);
       }      
    }      
    res.json( response );   
+});
+
+router.patch('/:id', async function(req, res){
+   console.log(`PATCH route on season ${req.params.id}, params: ${req.body}`);
+   let seasonDoc = await Season.findById( req.params.id );                     
+   // TODO update label and comment accoprdings to req.body.label, rey.body.comment
+   // save seasonDoc
+   // if boundaries != 0, do a lot of things...
+   // 1:    start one week later
+   // 2: start one week earlier -> check if not in collision with other Season and create week doc
+   // 3: finish one week later -> check in fno in collision and add new week doc
+   // 4: finish earlier: delete dpls and DienstExtRefs. Update DPLs' counting, seating docs
+   response = await addStat(seasonDoc);
+   res.send( response );
+   console.log(req.body);
+   console.log(response);
 });
 
 router.post('/', function(req, res){
