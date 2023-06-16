5bec3bb398bcc6347892f38d13918f1b3dccb919
Malaj <bicsak@gmx.net>
2023-06-16 23:27:36 +0200

    periods complete v0

--- routes/periods.js
+++ routes/periods.js
@@ -2,8 +2,10 @@ let express = require('express');
 let router = express.Router();
 const Period = require('../models/period');
 const Dpl = require('../models/dpl');
+const Orchestra = require('../models/orchestra');
 
 const { writeOperation } = require('../my_modules/orch-lock');
+const { DateTime } = require("luxon");
 
 /*****
  * Only for schedulers
@@ -39,7 +41,7 @@ router.get('/', async function(req, res) {
             s: req.authData.s,
             p: periodDocs[i]._id
          } ).sort('-weekBegin').limit(1).select('weekBegin');         
-         resp[resp.length - 1].lastDplBegin = lastDpl[0].weekBegin.getTime();
+         resp[resp.length - 1].lastDplBegin = lastDpl[0]?.weekBegin.getTime();
       }
     }
     console.log(resp);
@@ -48,9 +50,32 @@ router.get('/', async function(req, res) {
 
  async function deletePeriod(session, params) {    
    // params.o, params. sec, params.pId
-   // TODO check if period contains any dpls - if yes, abort
-   // TODO delete period from collection
-   // TODO set other periods' isOpenEnd and nextPBegin fields
+   // check if period contains any dpls - if yes, abort
+   let dplDoc = await Dpl.findOne({
+      o: params.o,
+      s: params.sec,
+      p: params.pId
+   }).session(session);
+   if ( dplDoc ) return false;
+
+   let periodDoc = await Period.findById(params.pId).session(session);
+   let nextPBegin = periodDoc.nextPBegin;
+   let nextP = periodDoc.nextP;
+   // delete period doc from collection
+   //await periodDoc.deleteOne(); // ??
+   await Period.findByIdAndRemove(params.pId).session(session);
+   
+   // set last period's isOpenEnd and nextPBegin, nextP (id) fields
+   let lastPeriodDoc = await Period.findOne({
+      nextP: params.pId
+   }).session(session);
+   if ( lastPeriodDoc.length ) {
+      lastPeriodDoc[0].nextP = nextP;
+      lastPeriodDoc[0].nextPBegin = nextPBegin;
+      lastPeriodDoc[0].isOpenEnd = !nextP;
+      await lastPeriodDoc[0].save();
+   }
+   
    return true;
  }
 
@@ -60,9 +85,14 @@ router.get('/', async function(req, res) {
        o: req.authData.o,  
        sec: req.authData.s,              
        pId: req.params.pId       
-    });             
-
-   res.json( result );
+    });    
+    if (result) res.status(202).send({
+      success: true,
+      content: 'Successfully removed period with id...'
+    }); else res.status(400).send({
+      success: false,
+      reason: 'Failed to remove period'
+    });
 });
 
 async function replacePeriodComment(session, params) {     
@@ -92,34 +122,121 @@ router.patch('/:pId', async function(req, res) {
    }      
 });
 
-
 async function createPeriod(session, params) {     
-   // TODO create period with date, comment and group; 
-   // check if operation is allowed; 
+   // create period with date, comment and group;
+   // check if operation is allowed 
    // update other periods' nextPBegin and isOpenEnd
    // return new Period
-   return true;
+   console.log('Creating new period... params:');
+   console.log(params);
+   //let dtBegin = new Date(params.begin*1000);
+   // must be a monday! get last monday for this date
+   let orchDoc = await Orchestra.findById(params.o);
+   let dtBegin = DateTime.fromMillis(params.begin*1000, {zone: orchDoc.timezone }).startOf('week').toJSDate();   
+   
+   let nextPeriodDoc = await Period.find({
+      begin: { '$gt': dtBegin }
+   }).session(session).sort({begin: 'asc'}).limit(1);
+   /*let nextPeriodDoc = await Period
+   .find({
+      o: params.o,
+      s: params.sec      
+   }).session(session).where('begin').gt(dtBegin)
+   .sort('begin').limit(1);*/
+   
+   let lastPeriodDoc = await Period.find({
+      begin: { '$lt': dtBegin }
+   }).session(session).sort({begin: 'desc'}).limit(1);
+   /*let lastPeriodDoc = await Period
+   .find({
+      o: params.o,
+      s: params.sec,
+   }).session(session).where('begin').lt(dtBegin)
+   .sort('-begin').limit(1);*/
+   console.log(lastPeriodDoc);
+
+   if ( lastPeriodDoc.length ) {
+      // get last dpl of this last period
+      let lastDplDoc = await Dpl.find({
+         o: params.o,
+         s: params.sec,
+         p: lastPeriodDoc[0]._id
+      }).session(session).sort('-begin').limit(1);
+
+      if ( lastDplDoc.length && lastDplDoc[0].begin > dtBegin ) {
+         // operation not allowed, abort:
+         return false;
+      }
+   }   
+   
+   const newPeriod = new Period( {
+      o: params.o,
+      s: params.sec,
+      begin: dtBegin,
+      next: nextPeriodDoc.length ? nextPeriodDoc[0]._id : undefined,
+      nextPBegin: nextPeriodDoc.length ? dtBegin : undefined,
+      isOpenEnd: !nextPeriodDoc.length,      
+      comment: params.comment,
+      members: params.group
+   });
+   await newPeriod.save( { session: session } );
+
+   if ( lastPeriodDoc.length ) {
+      lastPeriodDoc[0].isOpenEnd = false;
+      lastPeriodDoc[0].nextPBegin = dtBegin;
+      lastPeriodDoc[0].next = newPeriod._id;
+      await lastPeriodDoc[0].save();   
+   }
+
+   await newPeriod.populate('members.prof', 'userSn userFn userBirthday user');
+   // for all members populate profile: userSn, userFn, userBirthday, user (id)
+   
+   return {
+      ...newPeriod.toJSON(),
+      countDpl: 0
+   };
 }
  
- router.post('/', async function(req, res) {
-   //TODO    
-    console.log(req.body);
-    let result = await writeOperation( req.authData.o, createPeriod, {      
-      o: req.authData.o,       
-      sec: req.authData.s/*, TODO other params from req.body*/
-   });      
-   console.log(`Period created: ${result}`);      
-       
-   res.json( { success: false,
-      reason: "Not yet implemented" } );     
- });
+router.post('/', async function(req, res) {   
+   try {
+      let result = await writeOperation( req.authData.o, createPeriod, {      
+         o: req.authData.o,       
+         sec: req.authData.s,
+         begin: req.body.begin,
+         comment: req.body.comment,
+         group: req.body.group.map( (m, index) => {
+            return {
+               ...m,
+               factor: 1,
+               row: index,
+               prof: req.body.profileIds[index]
+            }
+         })
+      });      
+      console.log(`Period created, result of write operation: ${result}`); 
+      console.log(result);
+      console.log(result.members);
+      if ( !result ) res.status(409).send({
+         success: false,
+         reason: "Fehler"
+      }); else res.json( { 
+         success: true,
+         content: result 
+      } );     
+   }  catch (err) {
+      res.status(409).send({
+         success: false,
+         reason: err
+      });
+   }
+});
 
  async function editPeriodMember(session, params) {        
    let period = await Period.findById( params.pId ).session(session);
    period.members[params.row].canWish = params.memberData.canWish;
    period.members[params.row].canComment = params.memberData.canComment;
    period.members[params.row].initial = params.memberData.initial;
-   // tODO check if initial is unique in the group
+   // TODO check if initial is unique in the group
    period.members[params.row].start = params.memberData.start;
    if (params.memberData.comment) period.members[params.row].comment = params.memberData.comment;
    await period.save();   
@@ -145,7 +262,7 @@ async function createPeriod(session, params) {
    } catch(err) {
       res.status(409).send({
          success: false,
-         reason: 'Konflikt'
+         reason: err
       });
    }
    
