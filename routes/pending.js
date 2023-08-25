0a369be72f0220b3545667bc9e3cedda82e2fe2f
Malaj <bicsak@gmx.net>
2023-08-26 00:05:31 +0200

    pending requests aggregate

--- routes/pending.js
+++ routes/pending.js
@@ -6,21 +6,51 @@ const Dpl = require('../models/dpl');
 router.get('/', async function(req, res) { 
   console.log(req.authData);
     try {
-      let session = app.get('session');                   
-      let findConfig = {
-        o: req.authData.o              
-      };
+      let session = app.get('session');                         
+      let pendingDocs;
       if ( req.authData.r == 'musician' ) {
-        // TODO findConfig.groupSurvey.feedbacks        
-        findConfig.closed = true;
-      }
-      if ( req.authData.r == 'office' ) {
-        findConfig.published = true;
+        pendingDocs = await Dpl.aggregate( [
+          {
+            '$match': {
+              'o': mongoose.Types.ObjectId(req.authData.o), 
+              'closed': true, 
+              'published': false, 
+              's': req.authData.s
+            }
+          }, {
+            '$addFields': {
+              'memberIndex': {
+                '$indexOfArray': [
+                  '$periodMembers', mongoose.Types.ObjectId(req.authData.pid) 
+                ]
+              }
+            }
+          }, {
+            '$project': {
+              'status': {
+                '$arrayElemAt': [
+                  '$groupSurvey.feedbacks', '$memberIndex'
+                ]
+              }, 
+              'weekBegin': 1, 
+              s: 1,
+              '_id': 1
+            }
+          }, {
+            '$match': {
+              'status.vote': 'pending'
+            }
+          }
+        ]).session(session);        
+      } else if ( req.authData.r == 'office' ) {
+        let findConfig = {
+          o: req.authData.o,
+          published: true
+        };        
         findConfig['officeSurvey.status'] = 'pending';
-      }
-      console.log(findConfig);
-      let pendingDocs = await Dpl.find(findConfig).sort({'weekBegin': 1}).limit(5).session(session);
-      
+        console.log(findConfig);
+        pendingDocs = await Dpl.find(findConfig).sort({'weekBegin': 1}).limit(5).session(session);             
+      }      
       console.log(pendingDocs);
       let converted = pendingDocs.map( p => {
         return {
