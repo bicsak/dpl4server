3ea9d01b027ad7fd03a30ed2caf5d93379985b60
Malaj <bicsak@gmx.net>
2024-01-14 23:00:10 +0100

    calendar feed without description (seating info)

--- routes/calendar.js
+++ routes/calendar.js
@@ -4,110 +4,255 @@ const mongoose = require( 'mongoose' );
 const app = require('../server');
 const ics = require('ics');
 
-//const DienstExtRef = require('../models/dienst');
-const Dpl = require('../models/dpl');
-const Profile = require('../models/profile');
+const Orchestra = require('../models/orchestra');
+const Week = require('../models/week');
+/*const DienstExtRef = require('../models/dienst'); */
 
-const { DateTime } = require("luxon");
+const Profile = require('../models/profile');
+/* const { DateTime } = require("luxon"); */
 
 router.get('/', async function(req, res) {   
   // req.query.profId, req.query.type == 'all'|'dienst'|'no-dienst'
     
     try {
       let session = app.get('session');
-      let prof = req.query.profId;
-      let profDoc = await Profile.findById(prof).session(session).populate('o');
+      let prof = req.query.profId;      
+      let profDoc = await Profile.findById(prof).session(session);
+      if ( profDoc.role != 'musician' ) {
+        res.status(500).send('Calendar feed not allowed');
+       return;
+      }
       let orch = profDoc.o;
-      let result = await Dpl.aggregate( [
+      let orchDoc = await Orchestra.findById(orch).session(session);
+      if ( !orchDoc.calendar ) {
+        res.status(500).send('Calendar feed not allowed');
+       return;
+      }
+
+      let d = new Date();
+      d.setDate(d.getDate() - 21); // date 3 weeks ago
+      let result = await Week.aggregate(
+      [
         {
-          '$match': { 
-            //'o': mongoose.Types.ObjectId(req.authData.o), 
-            'weekBegin': { '$gte': DateTime.now().startOf('week').toJSDate() },
-            'periodMembers': mongoose.Types.ObjectId(prof)
+          '$match': {
+            'o': mongoose.Types.ObjectId(orch), 
+            'begin': {
+              '$gte': d 
+            }
           }
-        }, 
-        { '$addFields': { 'memberInd': { '$indexOfArray': [ '$periodMembers', mongoose.Types.ObjectId(prof) ] } } }, 
-        { '$unwind': { 'path': '$seatings' } },         
-        { '$addFields': { 'disp': { '$arrayElemAt': [ '$seatings.sp', '$memberInd' ] } } }, 
-        { '$match': { 
-          'seatings.dienstBegin': {'$gt': DateTime.now().toJSDate()},
-          'disp': { $in: [1, 16] } // TODO which req.query.type all/no-dienst/dienst
-        } 
-          // TODO if Krank...
-        }, 
-        { '$sort': { 'seatings.dienstBegin': 1 } }, 
-        //{ '$limit': 3 },
-        { '$lookup': { from: 'dienst', localField: 'seatings.d', foreignField: '_id', as: 'dienst' } },
-        // tODO lookup production for duration
-        // lookup period for initials??
-        { '$project': {    
-          'published': 1,
-          'weekBegin': 1,
-          'dienst': { $arrayElemAt: ["$dienst", 0] }
+        }, {
+          '$unwind': {
+            'path': '$dienst'
+          }
+        }, {
+          '$addFields': {
+            'dienst.wId': '$_id', 
+            'dienst.weekRemark': '$remark',
+            'dienst.weekBegin': '$begin'
+          }
+        }, {
+          '$replaceRoot': {
+            'newRoot': '$dienst'
+          }
+        }, /*{
+          '$match': {
+            'begin': {
+              '$gte': new Date() // ?? do we need it?
+            }
+          }
+        }, */{
+          '$lookup': {
+            'from': 'productions', 
+            'localField': 'prod', 
+            'foreignField': '_id', 
+            'as': 'prod', 
+            'pipeline': [
+              {
+                '$project': {
+                  'duration': 1
+                }
+              }
+            ]
+          }
+        }, {
+          '$addFields': {
+            'prodDuration': {
+              '$getField': {
+                'field': 'duration', 
+                'input': {
+                  '$arrayElemAt': [
+                    '$prod', 0
+                  ]
+                }
+              }
+            }
+          }
+        }, {
+          '$lookup': {
+            'from': 'dpls', 
+            'localField': 'wId', 
+            'foreignField': 'w', 
+            'as': 'dpl', 
+            'let': {
+              'dienstid': '$_id'
+            }, 
+            'pipeline': [
+              {
+                '$match': {
+                  's': 'sec5', 
+                  'periodMembers': mongoose.Types.ObjectId(prof)
+                }
+              }, {
+                '$unwind': {
+                  'path': '$seatings'
+                }
+              }, {
+                '$addFields': {
+                  'seating_did': '$seatings.d', 
+                  'dienst_did': '$$dienstid'
+                }
+              }, {
+                '$match': {
+                  '$expr': {
+                    '$eq': [
+                      '$seating_did', '$dienst_did'
+                    ]
+                  }
+                }
+              }, {
+                '$addFields': {
+                  'memberInd': {
+                    '$indexOfArray': [
+                      '$periodMembers', mongoose.Types.ObjectId(prof)
+                    ]
+                  }
+                }
+              }
+            ]
+          }
+        }, {
+          '$lookup': {
+            'from': 'periods', 
+            'localField': 'dpl.0.p', 
+            'foreignField': '_id', 
+            'as': 'period'
+          }
+        }, {
+          '$addFields': {
+            'dpl': {
+              '$arrayElemAt': [
+                '$dpl', 0
+              ]
+            }, 
+            'period': {
+              '$arrayElemAt': [
+                '$period', 0
+              ]
+            }
+          }
+        }, {
+          '$project': {
+            'name': 1, 
+            'col': 1, 
+            'begin': 1, 
+            'category': 1, 
+            'subtype': 1, 
+            'weight': 1, 
+            'duration': 1, 
+            'location': 1, 
+            'instrumentation': 1, 
+            'suffix': 1,
+            'seq': 1, 
+            'total': 1, 
+            'prodDuration': 1, 
+            'weekRemark': 1, 
+            'weekBegin': 1,
+            'dpl.seatings': 1, 
+            'dpl.closed': 1, 
+            'dpl.published': 1, 
+            'dpl.remark': 1, 
+            'dpl.memberInd': 1, 
+            'dpl.absent': 1, 
+            'period.members': 1
           }
         }
-      ]).session(session);
-    console.log('Aggregation result:', result);
-    /*let response = result.map( val => {
-      return {
-        weekBegin: val.weekBegin.getTime(),
-        published: val.published,
-        name: val.dienst.name,
-        category: val.dienst.category,
-        subtype: val.dienst.subtype,
-        seq: val.dienst.seq,
-        total: val.dienst.total,
-        suffix: val.dienst.suffix,
-        dienstBegin: val.dienst.begin.getTime()
-      }
+      ]
+      ).session(session);
+    //console.log('Aggregation result:', result);   
+    
+    let events = [];
+    //let url = req.get('host');
+    let url = "https://odp.bicsak.net"; // TODO
+    console.log(url);
+    console.log(req.query.type);
+    for ( let i = 0; i < result.length; i++ ) {
+      let dienst = result[i];
+      let dpl = result[i].dpl;
+      let hasDienst =  dpl &&
+       (dpl.seatings[dpl.memberInd] == 16 || dpl.seatings[dpl.memberInd] == 1) && 
+       dpl.absent[dienst.col][dpl.memberInd] == 0;
+      if ( req.query.type == 'all' ||
+        req.query.type == 'dienst' && hasDienst ||
+        req.query.type == 'no-dienst' && !hasDienst ) {
+        let duration = dienst.duration ? dienst.duration : dienst.prodDuration;                 
+        let name = dienst.name;
+        let suffix = orchDoc.categories[dienst.category].suffixes[dienst.subtype];
+        if (suffix) name += " " + suffix;        
+        if ( dienst.category == 0 && dienst.subtype == 6 ) name += " " + dienst.suffix;
+        if ( dienst.category == 1 ) name = name.toUpperCase();
+        if ( orchDoc.categories[dienst.category].numbers[dienst.subtype] ) {
+          // add numbering
+          if ( dienst.seq < 1 ) name += '#';
+          name += Math.abs(dienst.seq);
+          if ( orchDoc.lastPerformance && dienst.category == 2 && dienst.seq == dienst.total ) name += ' z.l.M.';
+        }
+        let event = {
+          productId: 'ODP',
+          start: [dienst.begin.getUTCFullYear(), dienst.begin.getUTCMonth()+1, dienst.begin.getUTCDate(), dienst.begin.getUTCHours(), dienst.begin.getUTCMinutes()], 
+          startInputType: 'utc',
+          duration: { minutes: duration },
+          title: name,
+          description: 'Status:, Genehmigung:, Einteilung, Aushilfen:, Dienstkommentar von DE, OD, Freiwünsche:, Einteilung OK?', // TODO
+          location: dienst.location.full,
+          url: `${url}/musician/week/?mts=${dienst.weekBegin.getTime()}`,
+          status: dpl?.published ? 'CONFIRMED' : 'TENTATIVE'           
+        };
+        //console.log(event);
+        events.push(event);
+      }      
+    }
+    //console.log(events);
+    const { error, value } = ics.createEvents(events);
+     /*[
+       {
+         title: 'Lunch',
+         start: [2023, 11, 15, 12, 15],
+         duration: { minutes: 45 }
+       },
+       {
+         title: 'Dinner',
+         start: [2023, 11, 15, 12, 15],
+         duration: { hours: 1, minutes: 30 }
+       }
+     ]*/     
+     if (error) {
+      console.log(error);
+      return;
+    }
+    
+    console.log(value);
+    res.set({
+      'Content-Type': 'text/calendar',        
+      'Content-Disposition': `attachment; filename=odp_dienste.ics`,
     });
-    console.log('Converted:', response);
-    res.status(200).json(response);  */     
+    res.send(value);
+
     } catch (err) {
       console.log(err);
        res.status(500).send(err.message);
        return;
-    }
-
-    const { error, value } = ics.createEvents(
-       result.map(dpl => {
-        let event = {
-          start: [dpl.dienst.begin.getUTCFullYear(), dpl.dienst.begin.getUTCMonth()+1, dpl.dienst.begin.getUTCDate(), dpl.dienst.begin.getUTCHours(), dpl.dienst.begin.getUTCMinutes()], 
-          startInputType: 'utc',
-          duration: { hours: 3, minutes: 0 }, // TODO from orch or production
-          title: dpl.dienst.name, // TODO + subtype, suffix, seq toUpperCase, zLM etc.
-          description: 'Status:, Genehmigung:, Einteilung, Aushilfen:, Dienstkommentar von DE, OD, Freiwünsche:, Einteilung OK?', // TODO
-          location: 'Wiesbaden GH', // TODO dpl.location or from orch.categories
-          url: `https://odp.bicsak.net/musician/week/?mts=${dpl.weekBegin.getTime()}`,  // TODO url from env
-          status: dpl.published ? 'CONFIRMED' : 'TENTATIVE', 
-          productID: 'ODP'
-        }
-        return event;
-      })
-      /*[
-        {
-          title: 'Lunch',
-          start: [2023, 11, 15, 12, 15],
-          duration: { minutes: 45 }
-        },
-        {
-          title: 'Dinner',
-          start: [2023, 11, 15, 12, 15],
-          duration: { hours: 1, minutes: 30 }
-        }
-      ]*/);
+    }          
       
-      if (error) {
-        console.log(error);
-        return;
-      }
-      
-      console.log(value);
-      res.set({
-        'Content-Type': 'text/calendar',        
-        'Content-Disposition': `attachment; filename=odp_dienste.ics`,
-      });
-      res.send(value);
  });
 
  
